c33556006ffd619aa660aa70c64de964f43e4fa7
pedro-toodoo <pedro.abritta@toodoo.com.br>
2021-12-17 16:39:54 -0300

    while add

--- Atividade/testecliente.py
+++ Atividade/testecliente.py
@@ -1,6 +1,7 @@
 import requests
 URL = 'http://127.0.0.1:8080/'
 
-r = requests.get(URL)
+while True:
+    r = requests.get(URL)
+    print(r.text)
 
-print(r.text)
--- Atividade/testeserv.py
+++ Atividade/testeserv.py
@@ -14,15 +14,15 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):        
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-       # self.wfile.write("fr".format(self.path).encode('utf-8'))        
+       # self.wfile.write("fr".format(self.path).encode('utf-8'))
         self.wfile.write(input('R=').format(self.path).encode('utf-8'))
+        print()
             
 
     def do_Post(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
