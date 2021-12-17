0512525053f29cdcdc2512945228c645215bc56e
Daniloccruz <94702374+Daniloccruz@users.noreply.github.com>
2021-12-17 14:50:45 -0300

    Add files via upload

---
+++ Atividade/testecliente.py
@@ -0,0 +1,5 @@
+import requests
+URL = 'http://127.0.0.1:8080'
+r = requests.get(URL)
+
+print(r.content)
---
+++ Atividade/testeserv.py
@@ -0,0 +1,40 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+hostName = "127.0.0.1"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        
+    def do_GET(self):        
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        
+
+    def do_Post():
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
